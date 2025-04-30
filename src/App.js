912800367e99d50f7a427b0ade5a66a245055993
maggi <margarita.herazo.i@gmail.com>
2025-04-30 10:45:27 -0300

    adding router, routes and pages in my App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Testimonials from './pages/Testimonials';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </Router>
   );
 }
 
