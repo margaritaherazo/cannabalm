c5911aeb7edbda50f4fc62bd66fa56ac49540a3c
maggi <margarita.herazo.i@gmail.com>
2025-05-05 09:37:13 -0300

    adding testimonial example

---
+++ src/pages/Testimonials.js
@@ -0,0 +1,12 @@
+
+function Testimonials() {
+    return (
+      <section>
+        <h1>What People Say</h1>
+        {/* Add testimonial cards here */}
+      </section>
+    );
+  }
+  
+  export default Testimonials;
+  
