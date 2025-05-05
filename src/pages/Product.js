c2e699065ffa388e96ef7cb168dba3d296e92769
maggi <margarita.herazo.i@gmail.com>
2025-05-05 10:07:07 -0300

    adding product example

---
+++ src/pages/Product.js
@@ -0,0 +1,11 @@
+function Product() {
+    return (
+      <section>
+        <h1>Our Product</h1>
+        {/* Add product description, images, maybe a buy button */}
+      </section>
+    );
+  }
+  
+  export default Product;
+  
