7655dc97ac7dba9c95dbdb03f630177846571d69
maggi <margarita.herazo.i@gmail.com>
2025-05-05 10:00:36 -0300

    adding Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Testimonials from './pages/Testimonials';
 import Product from './pages/Product';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/testimonials" element={<Testimonials />} />
