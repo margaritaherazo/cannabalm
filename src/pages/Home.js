3aa6b3374ddf277228ccce0e87dc0e60a116a596
maggi <margarita.herazo.i@gmail.com>
2025-05-05 09:36:16 -0300

    adding components to home

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import Info from '../components/Info';
+import About from '../components/About';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Info />
+      <About />
+    </>
+  );
+}
+
+export default Home;
